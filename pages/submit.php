7bbfba5abaa207e1a13de3f56ce84854f6ab73fb
Taylor Hornby <havoc@defuse.ca>
2013-06-07 14:03:54 -0600

    Clarify the wording for "lack of truth" etc.

--- pages/submit.php
+++ pages/submit.php
@@ -6,14 +6,32 @@
     <div class="boxcontent">
 
     <p>
-    What sets Hypothetico apart from other e-zines is our <em>lack</em> of
-    regard for the truth. An interesting idea in its raw form, not
-    constrained by the requirement of truth or rationality, can lead to new
-    insights in existing fields, and even the creation of completely new
-    fields of study. Of course, once the new insights are made, they should
-    be subjected to reason and scientific rigor if they are to be used in
-    practice. Hypothetico acts as a &quot;safe zone&quot; for the
-    early publication strange, even controversial, ideas.
+    What sets Hypothetico apart from other e-zines is that we <em>do not</em>
+    require submitted articles to have undergone any kind of peer review
+    process. We do not even require submitted articles to contain a truth, or to
+    be rational. If you have an interesting idea, and you'd like smart people to
+    consider it, but you're not sure if it's already been done, or if it's
+    fundamentally flawed, then Hypothetico is the perfect place to have it
+    published. Hypothetico acts as a &quot;safe zone&quot; for the premature
+    publication of strange and controversial ideas.
+    </p>
+
+    <p>
+    An interesting idea in its raw form, not constrained by the requirement of
+    truth or rationality, can lead to new insights in existing fields, and even
+    the creation of completely new fields of study. For example, if you have
+    a strong argument against something that is widely held to be true, or even
+    proven to be true, allowing others to study and refute your argument can
+    give them a better understanding of why that thing is true, at a deeper
+    level. 
+    </p>
+
+    <p>
+    This is not to say that the scientific method and mathematical rigor are not
+    important. Hypothetico readers must recognize that the published works are
+    intended only to provoke thought, and that any insights they get from
+    thinking about them must still be rigorously tested before they are used in
+    practice or accepted as true.
     </p>
 
     <p>
@@ -34,16 +52,16 @@
     no line is longer than 80 characters, and should not be longer than 200
     lines. The length requirement may seem harsh, but we are interested in
     thought-provoking ideas, and we want to leave the reader with time to think.
-    There is no minimum length. If you can do something really clever in one
-    line, we'll accept it.
+    There is no minimum length. If you can do something really clever in 80
+    characters, we'll accept it.
     </p>
 
     <p>
-    As always, please use correct spelling and grammar, unless not doing so is
-    an intentional component of your work. Your submission doesn't have to be
+    Please use correct spelling and grammar, unless not doing so is an
+    intentional component of your work. Your submission doesn't have to be
     English prose, though. It could be a poem, sequence of mathematical
-    equations, unique ASCII art, or even a nonsensical sequence of characters
-    that exhibits some interesting pattern.
+    equations, unique ASCII art, puzzle, or even a nonsensical sequence of
+    characters that exhibits some interesting pattern.
     </p>
 
     <p>
@@ -53,7 +71,7 @@
 
     <p>
     The submission deadline for Issue #1 is <b>January 1, 2014</b>. To submit
-    a peice, email it as a .txt attachment to <a
+    a piece, email it as a .txt attachment to <a
     href="mailto:havoc@defuse.ca">havoc@defuse.ca</a>. If you have a question
     please send it to the same address. Please include "Hypothetico" in the
     subject line.
